4a5a6109b74504f943c3d7ca1047aa2964a4912e
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-10-29 10:58:00 +0200

    added rn 67

--- replace.sh
+++ replace.sh
@@ -1,17 +1,20 @@
 tar zxvf react-native-reanimated*
 
+rm -rf ./rn67rc2/node_modules/react-native-reanimated/*
 rm -rf ./rn66/node_modules/react-native-reanimated/*
 rm -rf ./rn65/node_modules/react-native-reanimated/*
 rm -rf ./rn64/node_modules/react-native-reanimated/*
 rm -rf ./rn63/node_modules/react-native-reanimated/*
 rm -rf ./rn62/node_modules/react-native-reanimated/*
 
+cp -r ./package/* ./rn67rc2/node_modules/react-native-reanimated/
 cp -r ./package/* ./rn66/node_modules/react-native-reanimated/
 cp -r ./package/* ./rn65/node_modules/react-native-reanimated/
 cp -r ./package/* ./rn64/node_modules/react-native-reanimated/
 cp -r ./package/* ./rn63/node_modules/react-native-reanimated/
 cp -r ./package/* ./rn62/node_modules/react-native-reanimated/
 
+cd ./rn67rc2/ios && pod install && cd ../..
 cd ./rn66/ios && pod install && cd ../..
 cd ./rn65/ios && pod install && cd ../..
 cd ./rn64/ios && pod install && cd ../..
