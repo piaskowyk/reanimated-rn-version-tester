3fd4c87a92ff798dd5dcb4d8633bbecc7d5fc3ee
Kacper Kafara <kacperkafara@gmail.com>
2021-11-15 10:46:26 +0100

    Modify structure of replace.sh script

--- replace.sh
+++ replace.sh
@@ -3,22 +3,22 @@ tar zxvf react-native-reanimated*
 
 default_versions="67rc2 66 65 64 63 62"
 
+function replace() {
+    echo $1
+    rm -rf rn$1/node_modules/react-native-reanimated/*
+    cp -R package/* rn$1/node_modules/react-native-reanimated
+    cd rn$1/ios && pod install && cd ../../
+}
+
 if [[ $# -ge 1 ]]; then
-    for version in $@
-    do
-    echo $version
-        rm -rf rn${version}/node_modules/react-native-reanimated/*
-        cp -R package/* rn${version}/node_modules/react-native-reanimated
-        cd rn${version}/ios && pod install && cd ../../
-    done
+    versions=$@
 else
-    for version in ${default_versions}
-    do
-        echo $version
-        rm -rf rn${version}/node_modules/react-native-reanimated/*
-        cp -R package/* rn${version}/node_modules/react-native-reanimated
-        cd rn${version}/ios && pod install && cd ../../
-    done
+    versions=$default_versions
 fi
+
+for version in $versions
+do
+    replace $version
+done
 
 rm -fr package
