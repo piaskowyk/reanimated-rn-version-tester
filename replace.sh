42cea03362a8318c661ddcab831ac90cc6ce0826
Kacper Kafara <kacperkafara@gmail.com>
2021-11-15 10:31:32 +0100

    Fix replace.sh
    Changed default_versions variable type from array to string, it makes
    easier to iterate

--- replace.sh
+++ replace.sh
@@ -1,21 +1,23 @@
 rm -rf package
 tar zxvf react-native-reanimated*
 
-default_versions=("67rc2" "66" "65" "64" "63" "62")
+default_versions="67rc2 66 65 64 63 62"
 
 if [[ $# -ge 1 ]]; then
     for version in $@
     do
-        rm -rfv ./rn${version}/node_modules/react-native-reanimated/*
-        cp -Rv ./package/ ./rn${version}/node_modules/react-native-reanimated/
-        cd ./rn${version}/ios && pod install && cd ../../
+    echo $version
+        rm -rf rn${version}/node_modules/react-native-reanimated/*
+        cp -R package/* rn${version}/node_modules/react-native-reanimated
+        cd rn${version}/ios && pod install && cd ../../
     done
 else
-    for version in default_versions
+    for version in ${default_versions}
     do
-        rm -rfv ./rn${version}/node_modules/react-native-reanimated/*
-        cp -Rv ./package/ ./rn${version}/node_modules/react-native-reanimated/
-        cd ./rn${version}/ios && pod install && cd ../../
+        echo $version
+        rm -rf rn${version}/node_modules/react-native-reanimated/*
+        cp -R package/* rn${version}/node_modules/react-native-reanimated
+        cd rn${version}/ios && pod install && cd ../../
     done
 fi
 
