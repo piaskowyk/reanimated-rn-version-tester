43b020f0033737e9ec5d1c1b5e22ebe724eb2953
Kacper Kafara <kacperkafara@gmail.com>
2021-11-15 10:28:05 +0100

    Add rn66 & rn67rc2 to setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-cd ./rn65/ios && yarn && cd ../..
-cd ./rn64/ios && yarn && cd ../..
-cd ./rn63/ios && yarn && cd ../..
-cd ./rn62/ios && yarn && cd ../..
+default_versions="67rc2 66 65 64 63 62"
+
+for version in ${default_versions}
+do
+    echo "Setting up rn${version}"
+    cd rn${version} && yarn && cd ..
+done
